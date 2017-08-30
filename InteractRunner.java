3b0ad0206a725ccea439fde425dd23fe163e1bb1
rustem93seytasanov <rustem_simf@outlook.com>
2017-08-30 20:34:11 +0300

    input

--- InteractRunner.java
+++ InteractRunner.java
@@ -10,31 +10,61 @@ public class InteractRunner {
 			while (!exit.equals("yes")){
 				System.out.println("Vvedite deystvie(+,-,/,*)");
 				deystvie = reader.next();
+				/**
+				проверка на правильность введенного оператора
+				если оператор введен неправильно - программа не выполнит оперцаий
+				и выведет сообщение об ошибке
+				*/
 				if("+".equals(deystvie)||"*".equals(deystvie)||"/".equals(deystvie)||"-".equals(deystvie))
 				{
+					/**
+					ввод значений для арифметических операций
+					*/
 				System.out.println("Enter first arg : ");
 				String first = reader.next();
 				System.out.println("Enter second arg : ");
 				String second = reader.next();
+				/**
+				вызов метода который осуществляет сложение
+				вызов метода который возвращает результат
+				выводит результат на экран
+				*/
 				if ("+".equals(deystvie))
 				{
 					calc.add(Integer.valueOf(first), Integer.valueOf(second));
 					System.out.println("Result : "+calc.getResult());
 				}
-				
+				/**
+				вызов метода который осуществляет умножение
+				вызов метода который возвращает результат
+				выводит результат на экран
+				*/
 				else if ("*".equals(deystvie)){
 					calc.umnoj(Integer.valueOf(first), Integer.valueOf(second));
 					System.out.println("Result : "+calc.getResult());
 				}
+				/**
+				вызов метода который осуществляет деление
+				вызов метода который возвращает результат
+				выводит результат на экран
+				*/
 				else if ("/".equals(deystvie)){
 					calc.div(Integer.valueOf(first), Integer.valueOf(second));			
 					System.out.println("Result : "+calc.getResult());										
 				}
+				/**
+				вызов метода который осуществляет вычитание
+				вызов метода который возвращает результат
+				выводит результат на экран
+				*/
 				else if ("-".equals(deystvie)){
 					calc.razn(Integer.valueOf(first), Integer.valueOf(second));
 					System.out.println("Result : "+calc.getResult());	
 				}
-				
+				/**
+				очищает результат операции
+				предлагает выйти из программы
+				*/
 				calc.cleanReasult();
 				System.out.println("Exit yes/no");
 				exit = reader.next();
